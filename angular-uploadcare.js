b630d97e6b906080f8dd9da678da412a41afd7c8
Eric C. Brown <djheir@gmail.com>
2014-07-15 12:16:57 -0500

    Initial commit

---
+++ angular-uploadcare.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name angular-uploadcare.directive:UploadCare
+ * @description Provides a directive for the Uploadcare widget.
+ * # UploadCare
+ */
+angular.module('ng-uploadcare')
+  .directive('uploadcare-widget', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      require: 'ngModel',
+      template: '<input type="hidden" role="ng-uploadcare-uploader" />',
+      scope: {
+        onUploadComplete: '&',
+        onWidgetReady: '&',
+      },
+      controller: ['$scope', function($scope) {
+        $scope.widget = uploadcare.Widget('[role=ng-uploadcare-uploader]');
+        $scope.onWidgetReady({widget: $scope.widget});
+        $scope.widget.onUploadComplete(function(info) {
+          $scope.onUploadComplete({info: info});
+        });
+      }]
+    };
+  });
