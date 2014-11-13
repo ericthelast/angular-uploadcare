48ff97b172c1c186cbcdacb817f3866a10a1fd58
homm <homm86@gmail.com>
2014-11-13 19:22:31 +0300

    Use current element for widget instead

--- angular-uploadcare.js
+++ angular-uploadcare.js
@@ -18,12 +18,12 @@ angular.module('ng-uploadcare', [])
         onUploadComplete: '&',
         onChange: '&',
       },
-      controller: ['$scope', '$log', function($scope, $log) {
+      controller: ['$scope', '$element', '$log', function($scope, $element, $log) {
         if(!uploadcare) {
           $log.error('Uploadcare script has not been loaded!.');
           return;
         }
-        $scope.widget = uploadcare.Widget('[role=ng-uploadcare-uploader]');
+        $scope.widget = uploadcare.Widget($element);
         $scope.onWidgetReady({widget: $scope.widget});
         $scope.widget.onUploadComplete(function(info) {
           $scope.onUploadComplete({info: info});
